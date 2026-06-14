e3dc3fb4e1eb1b9c65ed1808edd1a2c524a0140a
Daniil K <danilkarachev234@mail.ru>
2026-06-14 05:31:07 +0500

    update

--- client/src/store/slices/filterSlice.ts
+++ client/src/store/slices/filterSlice.ts
@@ -1,11 +1,14 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { StatusType } from "../../types";
 
 interface FiltersState {
   searchValue: string;
+  taskStatus: null | StatusType;
 }
 
 const initialState: FiltersState = {
   searchValue: "",
+  taskStatus: null,
 };
 
 export const filterSlice = createSlice({
@@ -15,9 +18,12 @@ export const filterSlice = createSlice({
     setSearchValue: (state, action: PayloadAction<string>) => {
       state.searchValue = action.payload;
     },
+    setStatusType(state, action: PayloadAction<null | StatusType>) {
+      state.taskStatus = action.payload;
+    },
   },
 });
 
-export const { setSearchValue } = filterSlice.actions;
+export const { setSearchValue, setStatusType } = filterSlice.actions;
 
 export default filterSlice.reducer;
