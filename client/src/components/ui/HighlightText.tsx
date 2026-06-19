0eeccddd5c5ea6b095d379e070933d4c580d9d71
Daniil K <danilkarachev234@mail.ru>
2026-06-19 05:02:15 +0500

    update

--- client/src/components/ui/HighlightText.tsx
+++ client/src/components/ui/HighlightText.tsx
@@ -7,11 +7,18 @@ interface IProps {
 }
 function HighlightText({ text, className }: IProps) {
   const { searchValue } = useFilters();
-  const index = text.indexOf(searchValue);
+  const index = text
+    .toLowerCase()
+    .trim()
+    .indexOf(searchValue.toLowerCase().trim());
+
+  if (!searchValue || index === -1) {
+    return <p className={`${className}`}>{text}</p>;
+  }
   return (
     <p className={`${className}`}>
       {text.slice(0, index)}
-      <span className="bg-yellow-100 font-medium">
+      <span className="bg-yellow-100 font-semibold">
         {text.slice(index, index + searchValue.length)}
       </span>
       {text.slice(index + searchValue.length)}
